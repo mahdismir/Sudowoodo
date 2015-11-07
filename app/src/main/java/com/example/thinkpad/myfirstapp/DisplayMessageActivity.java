3d6004cbe25e29b3083cd94637dc027816ac8144
Mitchell Topete <mitcho85@gmail.com>
2015-11-07 12:46:57 -0800

    Connected weather button to Arash work on main menu

--- app/src/main/java/com/example/thinkpad/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/thinkpad/myfirstapp/DisplayMessageActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -16,6 +18,25 @@ public class DisplayMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //Intent intent = getIntent();
         setContentView(R.layout.activity_display_message);
+        /******************************************/
+        /************WEATHER BUTTON****************/
+        /******************************************/
+        Button btnWeatherScreen = (Button) findViewById(R.id.btnWeatherScreen);
+        btnWeatherScreen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                //Starting a new Intent
+                Intent nextScreen = new Intent(getApplicationContext(), WeatherActivity.class);
+
+                //Sending data to another Activity
+                //nextScreen.putExtra("name", inputName.getText().toString());
+                //nextScreen.putExtra("email", inputEmail.getText().toString());
+
+                //Log.e("n", inputName.getText() + "." + inputEmail.getText());
+
+                startActivity(nextScreen);
+
+            }
+        });
     }
 
     public void sendMessage1(View view) {
--- app/src/main/java/com/example/thinkpad/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/thinkpad/myfirstapp/MyActivity.java
@@ -25,24 +25,9 @@ public class MyActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ImageButton btnNextScreen = (ImageButton) findViewById(R.id.btnNextScreen);
 
-        //listening to button event
-        btnNextScreen.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View arg0){
-                //Starting a new Intent
-                Intent nextScreen = new Intent(getApplicationContext(), WeatherActivity.class);
 
-                //Sending data to another Activity
-                //nextScreen.putExtra("name", inputName.getText().toString());
-                //nextScreen.putExtra("email", inputEmail.getText().toString());
 
-                //Log.e("n", inputName.getText() + "." + inputEmail.getText());
-
-                startActivity(nextScreen);
-
-            }
-        });
        // FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         //fab.setOnClickListener(new View.OnClickListener() {
         //    @Override
