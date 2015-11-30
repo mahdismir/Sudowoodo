9824654dbdad7909914a1872cbb4dfe0c9a3ed67
mahdismir <mmirmosa@ucsd.edu>
2015-11-30 11:29:21 -0800

    notification

--- app/src/main/java/com/example/thinkpad/myfirstapp/AlertReceiver.java
+++ app/src/main/java/com/example/thinkpad/myfirstapp/AlertReceiver.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
+import com.example.thinkpad.myfirstapp.MainMenuActivities.WateringScheduleActivities.WateringScheduleActivity;
 import com.example.thinkpad.myfirstapp.MainMenuActivities.WeatherActivity;
 
 /**
@@ -18,29 +19,16 @@ public class AlertReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent ){
-        String s;
-        int temperature = WeatherActivity.temperature;
-
-        // HOT temperature
-        if(temperature>=80)
-            s="Hot temperature!! Check your watering Instruction for appropriate watering";
-        // Normal temperature
-        else if(temperature>=50 && temperature<80)
-            s="Normal temperature!! Check your watering Instruction for appropriate watering";
-        //Cold temperature(less than 50)
-        else
-            s="Cold temperature!! Check your watering Instruction for appropriate watering";
 
 
-
-        String message = "Hellooo, alrm worked ----";
+        String message = "Hello, alrm worked ----";
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
 
-        createNotification(context, "Sudowoodo", s, "Load The App");
+        createNotification(context, "Cloudy today", "water your plants", "Load The App");
     }
 
     public void createNotification(Context context, String msg, String msgText, String msgAlert){
-        PendingIntent notificIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0);
+        PendingIntent notificIntent = PendingIntent.getActivity(context, 0, new Intent(context, WateringScheduleActivity.class), 0);
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.icon_29)
                 .setContentTitle(msg)
